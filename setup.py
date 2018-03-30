f5dcde482dfbb4bfb18bfae2867449bbe2b5543c
errantlinguist <errantlinguist+github@gmail.com>
2018-03-30 16:42:28 -0400

    updated description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='storygenerator_preprocessing',
       version='0.1',
-      description='Reads in literature and uses it to generate new data using a neural network.',
+      description='Preprocessing tools for data used for story generation using deep learning.',
       url='https://github.com/errantlinguist/storygenerator-preprocessing',
       author='Todd Shore',
       author_email='errantlinguist+github@gmail.com',
