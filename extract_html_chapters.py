1fa3be334a614bb408165f9f1b66529a1d3709bc
errantlinguist <errantlinguist+github@gmail.com>
2018-04-01 10:51:54 -0400

    made HTML file-walking logic object-oriented

--- extract_html_chapters.py
+++ extract_html_chapters.py
@@ -17,25 +17,26 @@ from typing import Iterable, Iterator
 from storygenerator_preprocessing import natural_keys
 from storygenerator_preprocessing.io import HTMLChapterReader, write_chapters
 
-HTML_FILE_EXTENSION_PATTERN = re.compile("\.x?html?", re.IGNORECASE)
 
+class HTMLFileWalker(object):
+	HTML_FILE_EXTENSION_PATTERN = re.compile("\.x?html?", re.IGNORECASE)
 
-def is_html_file(path: str) -> bool:
-	ext = os.path.splitext(path)[1]
-	match = HTML_FILE_EXTENSION_PATTERN.match(ext)
-	return bool(match)
+	@classmethod
+	def is_html_file(cls, path: str) -> bool:
+		ext = os.path.splitext(path)[1]
+		match = cls.HTML_FILE_EXTENSION_PATTERN.match(ext)
+		return bool(match)
 
-
-def walk_html_files(inpaths: Iterable[str]) -> Iterator[str]:
-	for inpath in inpaths:
-		if os.path.isdir(inpath):
-			for root, dirs, files in os.walk(inpath, followlinks=True):
-				for file in files:
-					filepath = os.path.join(root, file)
-					if is_html_file(filepath):
-						yield filepath
-		elif is_html_file(inpath):
-			yield inpath
+	def __call__(self, inpaths: Iterable[str]) -> Iterator[str]:
+		for inpath in inpaths:
+			if os.path.isdir(inpath):
+				for root, dirs, files in os.walk(inpath, followlinks=True):
+					for file in files:
+						filepath = os.path.join(root, file)
+						if self.is_html_file(filepath):
+							yield filepath
+			elif self.is_html_file(inpath):
+				yield inpath
 
 
 def __create_argparser() -> argparse.ArgumentParser:
@@ -47,9 +48,9 @@ def __create_argparser() -> argparse.ArgumentParser:
 						help="The directory to write the extracted book data to.", required=True)
 	log_args = result.add_mutually_exclusive_group()
 	log_args.add_argument("-i", "--info", help="increase output verbosity to INFO.",
-						action="store_true")
+						  action="store_true")
 	log_args.add_argument("-d", "--debug", help="increase output verbosity to DEBUG.",
-						action="store_true")
+						  action="store_true")
 	return result
 
 
@@ -61,9 +62,10 @@ def __main(args):
 
 	inpaths = args.inpaths
 	print("Will look for data under {}.".format(inpaths))
-	reader = HTMLChapterReader()
-	infiles = tuple(sorted(frozenset(walk_html_files(inpaths)), key=natural_keys))
+	file_walker = HTMLFileWalker()
+	infiles = tuple(sorted(frozenset(file_walker(inpaths)), key=natural_keys))
 	logging.info("Will read %d file(s).", len(infiles))
+	reader = HTMLChapterReader()
 	book_chapters = dict(reader(infiles))
 	print("Read data for {} book(s): {}".format(len(book_chapters), sorted(book_chapters.keys())))
 
